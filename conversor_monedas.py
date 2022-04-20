a536cb2d4fe7eb48301f59fe9e628583d46bb235
Victor Prado <vmpradob@gmail.com>
2022-04-19 23:32:59 -0400

    fix: adding menu to while para que continue el loop y usar eval para determinar si es True o False

--- conversor_monedas.py
+++ conversor_monedas.py
@@ -6,17 +6,18 @@ def bolivar_to_dolar(bolivar, equivalente) -> float:
 def dolar_to_bolivar(dolar, equivalente) -> float:
     return round(dolar * equivalente, 2)
 
-print("Escoja un tipo de conversión:")
-print("1. BsS-> USD")
-print("1. USD -> BsS")
-
-opcion = input("opción: ")
-quiero_convertir = True
-monto = input("Ingrese el monto a convertir: ")
-monto = float(monto)
-equivalente = 4.56
 
 while(quiero_convertir):
+    print("Escoja un tipo de conversión:")
+    print("1. BsS-> USD")
+    print("1. USD -> BsS")
+
+    opcion = input("opción: ")
+    quiero_convertir = True
+    monto = input("Ingrese el monto a convertir: ")
+    monto = float(monto)
+    equivalente = 4.56
+
     if(opcion == '1'):
         resultado = bolivar_to_dolar(monto, equivalente)
         print(f'el monto en dolares es: ${resultado}')
@@ -25,6 +26,6 @@ while(quiero_convertir):
         print(f'el monto en bolivares es: ${resultado}')
     else:
         print("la opción no es valida, escoja una de las alternativas (1, 2)")
-    quiero_convertir = bool(input("¿Quiere intentar otra conversión? (True,False)"))
+    quiero_convertir = eval(input("¿Quiere intentar otra conversión? (True,False)"))
 
 print("Gracias por usar la App")
