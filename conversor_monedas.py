48c03465581f96227353065db6e3594adaa40c87
Victor Prado <vmpradob@gmail.com>
2022-04-19 22:40:15 -0400

    conversor de monedas de bolivares a dolares

---
+++ conversor_monedas.py
@@ -0,0 +1,7 @@
+# Conversor de monedas de BsS a USD
+
+bolivar = input("¿Cuántos bolivares tienes?: ")
+bolivar = float(bolivar)
+valor_dolar = 4.56
+dolar = bolivar / valor_dolar
+print(f'el monto en dolares es: ${dolar}')
